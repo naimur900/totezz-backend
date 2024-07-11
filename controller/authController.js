983d5b34bd545b9638d192a795233f57d3d9c7c5
Naimur Rahman <naimur.rahman900@gmail.com>
2024-07-11 22:40:20 +0600

    refactored authController

--- controller/authController.js
+++ controller/authController.js
@@ -3,39 +3,36 @@ const CryptoJS = require("crypto-js");
 const User = require("../model/User");
 
 const signupUser = async (req, res) => {
-  const {
-    email,
-    password,
-    firstName,
-    lastName,
-    contactNumber,
-  } = req.body;
+  const { email, password, firstName, lastName, contactNumber } = req.body;
+
   try {
-    const user = await User.findOne({
+    const existingUser = await User.findOne({
       email: email,
     });
-    if (!user) {
-      const newUser = new User({
-        email: email,
-        password: CryptoJS.AES.encrypt(
-          password,
-          process.env.CRYPTO_SECRET
-        ).toString(),
-        firstName: firstName,
-        lastName: lastName,
-        contactNumber: contactNumber,
-      });
-      await newUser.save();
-      res.status(201).json({
-        status: true,
-        message: "User created successfully",
-      });
-    } else {
-      res.status(409).json({
+
+    if (existingUser) {
+      return res.status(409).json({
         status: false,
-        message: "User already exist",
+        message: "User already exists",
       });
     }
+
+    const newUser = new User({
+      email,
+      password: CryptoJS.AES.encrypt(
+        password,
+        process.env.CRYPTO_SECRET
+      ).toString(),
+      firstName,
+      lastName,
+      contactNumber,
+    });
+
+    await newUser.save();
+    res.status(201).json({
+      status: true,
+      message: "User created successfully",
+    });
   } catch (error) {
     res.status(500).json({
       status: false,
@@ -50,32 +47,34 @@ const signinUser = async (req, res) => {
     const user = await User.findOne({
       email: email,
     });
-    if (user) {
-      const decryptedPass = CryptoJS.AES.decrypt(
-        user.password,
-        process.env.CRYPTO_SECRET
-      ).toString(CryptoJS.enc.Utf8);
-      if (password === decryptedPass) {
-        const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-          expiresIn: "2h",
-        });
-        res.status(200).json({
-          status: true,
-          message: "User signed in",
-          firstName: user.firstName,
-          lastName: user.lastName,
-          token: token,
-        });
-      } else {
-        res.status(401).json({
-          status: false,
-          message: "Wrong password",
-        });
-      }
-    } else {
-      res.status(401).json({
+
+    if (!user) {
+      return res.status(404).json({
         status: false,
-        message: "Wrong credential",
+        message: "No such user found. Please sign up.",
+      });
+    }
+
+    const decryptedPass = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.CRYPTO_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (password === decryptedPass) {
+      const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+      return res.status(200).json({
+        status: true,
+        message: "User signed in",
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: token,
+      });
+    } else {
+      return res.status(401).json({
+        status: false,
+        message: "Wrong password",
       });
     }
   } catch (error) {
